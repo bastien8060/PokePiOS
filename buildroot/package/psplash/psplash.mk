f0a2f701b7230e7a8f5be482cfebb2b774833d3a
Bastien Saidi <saidi.ireland@gmail.com>
2021-10-07 11:09:33 +0100

    Added new boot logo

--- buildroot/package/psplash/psplash.mk
+++ buildroot/package/psplash/psplash.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PSPLASH_VERSION = 5b3c1cc28f5abdc2c33830150b48b278cc4f7bca
-PSPLASH_SITE = git://git.yoctoproject.org/psplash
+PSPLASH_VERSION = 20ac760f8a59b78c3c314528b51e200473e43702
+PSPLASH_SITE = git://github.com/bastien8060/PokePi_psplash
 PSPLASH_LICENSE = GPL-2.0+
 PSPLASH_LICENSE_FILES = COPYING
 PSPLASH_AUTORECONF = YES
